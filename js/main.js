f00effb9b9765ec4eff56019a03fcf2df04a2e6e
marmaromi <96768993+marmaromi@users.noreply.github.com>
2022-05-01 13:57:05 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ $(function () {
         $(`#searchBar`).html("");
         if (checkedToggles !== null && checkedToggles.length > 0) {
             $.ajax({
-                url: "../html/live-reports.html",
+                url: "https://marmaromi.github.io/project_2/html/live-reports.html",
                 success: show => $(`#sectionMain`).html(show),
                 error: err => console.log(err)
             });
@@ -34,7 +34,7 @@ $(function () {
         clearInterval(JSON.parse(localStorage.getItem("myInterval")));
         $(`#searchBar`).html("");
         $.ajax({
-            url: "../html/about.html",
+            url: "https://marmaromi.github.io/project_2/html/about.html",
             success: show => $(`#sectionMain`).html(show),
             error: err => console.log(err)
         });
